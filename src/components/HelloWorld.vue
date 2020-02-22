6b2bdc5b8a5b70142da0987a27279f2b985a3fbd
jdeyk <joserra_m26@live.com>
2020-02-22 09:33:34 -0600

    init 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title>
+        Arca de Noe
+      </v-toolbar-title>
+      <v-spacer/>
+
+      <v-btn icon @click="getWeb3"> <v-icon color="pruple">mdi-account</v-icon></v-btn>
+      {{this.cuenta ? this.cuenta[0] : ""}}
+      <v-btn icon ><v-icon color="pruple">mdi-wallet</v-icon></v-btn>
+    </v-app-bar>
+    <v-container>
+    </v-container>  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import web3 from 'web3';
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      cuenta: null,
+      balance: null
+    }),
+    methods: {
+      async getWeb3(){
+        await window.ethereum.enable(); // Iniciar conexion con metamastk
+        const instance = new web3(window.ethereum);
+        this.cuenta = await instance.eth.getAccounts(); // obtener la instancia
+
+        //Evento a la escucha
+        window.ethereum.on('accountsChanged', function(nuevas_cuentas){
+          this.cuentas = nuevas_cuentas;
+          console.log(this.cuentas);
+        });
+      }
+    }
+  }
+</script>
